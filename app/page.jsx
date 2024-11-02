9f088bc4df40cc8ab45c624220ed9b85075794ab
Nivetha Kuruparan <niv.kuru@gmail.com>
2024-11-02 18:59:37 -0400

    Added resume + removed Stats component

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
-import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -31,14 +30,19 @@ const Home = () => {
             </p>
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
+              <a
+                href="/assets/Nivetha_Kuruparan_Resume_2024.pdf"
+                download="Nivetha_Kuruparan_Resume"
               >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
@@ -53,7 +57,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/* <Stats /> */}
     </section>
   );
 };
